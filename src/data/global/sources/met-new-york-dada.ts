ca2f4340729c6a2e230b09ecfafdfea6c0eb0014
Bergtripper <albertocomini@gmail.com>
2026-09-25 21:24:02 +0200

    feat(dada): add met-new-york-dada.ts

---
+++ src/data/global/sources/met-new-york-dada.ts
@@ -0,0 +1,10 @@
+import { GlobalSource } from '../types';
+
+export const metNewYorkDada: GlobalSource = {
+  id: 'met-new-york-dada',
+  title: 'New York Dada',
+  publisher: 'The Metropolitan Museum of Art',
+  url: 'https://www.metmuseum.org/art/collection/search/923204',
+  sourceType: 'museum',
+  note: 'Used for the 1921 New York Dada publication edited by Marcel Duchamp and Man Ray.',
+};
