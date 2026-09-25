84bf4357ce8f6b4f58fe4e775789ba90f35ffd63
Bergtripper <albertocomini@gmail.com>
2026-09-25 21:24:40 +0200

    feat(dada): register transnational Dada routes

--- src/data/global/routes/index.ts
+++ src/data/global/routes/index.ts
@@ -1,20 +1,29 @@
 import { bayerBerlinNewYorkRoute } from './bayer-berlin-new-york';
+import { huelsenbeckZurichBerlinRoute } from './huelsenbeck-zurich-berlin';
+import { manRayNewYorkParisRoute } from './man-ray-new-york-paris';
 import { moholyNagyDessauChicagoRoute } from './moholy-nagy-dessau-chicago';
 import { murayamaBerlinTokyoRoute } from './murayama-berlin-tokyo';
 import { sharonDessauTelAvivRoute } from './sharon-dessau-tel-aviv';
 import { sternCoppolaBerlinBuenosAiresRoute } from './stern-coppola-berlin-buenos-aires';
 import { torresGarciaParisMontevideoRoute } from './torres-garcia-paris-montevideo';
+import { tzaraZurichParisRoute } from './tzara-zurich-paris';
 
 export {
   bayerBerlinNewYorkRoute,
+  huelsenbeckZurichBerlinRoute,
+  manRayNewYorkParisRoute,
   moholyNagyDessauChicagoRoute,
   murayamaBerlinTokyoRoute,
   sharonDessauTelAvivRoute,
   sternCoppolaBerlinBuenosAiresRoute,
   torresGarciaParisMontevideoRoute,
+  tzaraZurichParisRoute,
 };
 
 export const ALL_DIFFUSION_ROUTES = [
+  huelsenbeckZurichBerlinRoute,
+  tzaraZurichParisRoute,
+  manRayNewYorkParisRoute,
   murayamaBerlinTokyoRoute,
   sharonDessauTelAvivRoute,
   torresGarciaParisMontevideoRoute,
