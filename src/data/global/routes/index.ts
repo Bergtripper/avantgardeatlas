bc2ae917da9c050f49f3ad4fb7b4e0f3c1cba0c1
Bergtripper <albertocomini@gmail.com>
2026-09-25 21:10:44 +0200

    feat(global): register index.ts

--- src/data/global/routes/index.ts
+++ src/data/global/routes/index.ts
@@ -1,17 +1,26 @@
+import { bayerBerlinNewYorkRoute } from './bayer-berlin-new-york';
 import { moholyNagyDessauChicagoRoute } from './moholy-nagy-dessau-chicago';
 import { murayamaBerlinTokyoRoute } from './murayama-berlin-tokyo';
+import { sharonDessauTelAvivRoute } from './sharon-dessau-tel-aviv';
+import { sternCoppolaBerlinBuenosAiresRoute } from './stern-coppola-berlin-buenos-aires';
 import { torresGarciaParisMontevideoRoute } from './torres-garcia-paris-montevideo';
 
 export {
+  bayerBerlinNewYorkRoute,
   moholyNagyDessauChicagoRoute,
   murayamaBerlinTokyoRoute,
+  sharonDessauTelAvivRoute,
+  sternCoppolaBerlinBuenosAiresRoute,
   torresGarciaParisMontevideoRoute,
 };
 
 export const ALL_DIFFUSION_ROUTES = [
   murayamaBerlinTokyoRoute,
-  moholyNagyDessauChicagoRoute,
+  sharonDessauTelAvivRoute,
   torresGarciaParisMontevideoRoute,
+  sternCoppolaBerlinBuenosAiresRoute,
+  moholyNagyDessauChicagoRoute,
+  bayerBerlinNewYorkRoute,
 ];
 
 export const diffusionRoutesRegistry = Object.fromEntries(
