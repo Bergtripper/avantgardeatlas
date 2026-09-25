c3052a9b862c11cf422cb9c155588e92b16caf81
Bergtripper <albertocomini@gmail.com>
2026-09-25 17:35:07 +0200

    feat(global): add princeton-mavo.ts

---
+++ src/data/global/sources/princeton-mavo.ts
@@ -0,0 +1,10 @@
+import { GlobalSource } from '../types';
+
+export const princetonMavo: GlobalSource = {
+  id: 'princeton-mavo',
+  title: 'MAVO',
+  publisher: 'Princeton University Library',
+  url: 'https://dpul.princeton.edu/surrealism-at-one-hundred/feature/mavo',
+  sourceType: 'university',
+  note: 'Used for the magazine, its dates, print experimentation and relation to mass/commercial culture.',
+};
