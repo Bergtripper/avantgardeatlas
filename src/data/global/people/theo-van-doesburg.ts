ec861b63670377f42d5569c2fe466cef9a8bdaba
Bergtripper <albertocomini@gmail.com>
2026-09-25 22:45:03 +0200

    feat(print): add theo-van-doesburg.ts

---
+++ src/data/global/people/theo-van-doesburg.ts
@@ -0,0 +1,10 @@
+import { GlobalPerson } from '../types';
+
+export const theoVanDoesburg: GlobalPerson = {
+  id: 'theo-van-doesburg',
+  name: 'Theo van Doesburg',
+  years: '1883—1931',
+  summary:
+    'Dutch artist, designer, writer and editor who founded De Stijl magazine and used it as an increasingly international platform for European avant-garde exchange.',
+  sourceIds: ['moma-de-stijl-magazine', 'moma-van-doesburg'],
+};
