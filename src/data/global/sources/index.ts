ef1355244b2ef5b8b63bc57de1d240f8da474740
Bergtripper <albertocomini@gmail.com>
2026-09-25 21:10:47 +0200

    feat(global): register index.ts

--- src/data/global/sources/index.ts
+++ src/data/global/sources/index.ts
@@ -1,13 +1,23 @@
 import { bauhausNewBauhausChicago } from './bauhaus-new-bauhaus-chicago';
+import { bauhausPalestineSharon } from './bauhaus-palestine-sharon';
 import { kanagawaMurayama2011 } from './kanagawa-murayama-2011';
+import { momaBauhaus1938 } from './moma-bauhaus-1938';
+import { momaHerbertBayer } from './moma-herbert-bayer';
+import { momaSternCoppola } from './moma-stern-coppola';
 import { momaTorresGarcia } from './moma-torres-garcia';
 import { princetonMavo } from './princeton-mavo';
+import { unescoTelAvivWhiteCity } from './unesco-tel-aviv-white-city';
 
 export {
   bauhausNewBauhausChicago,
+  bauhausPalestineSharon,
   kanagawaMurayama2011,
+  momaBauhaus1938,
+  momaHerbertBayer,
+  momaSternCoppola,
   momaTorresGarcia,
   princetonMavo,
+  unescoTelAvivWhiteCity,
 };
 
 export const ALL_GLOBAL_SOURCES = [
@@ -15,6 +25,11 @@ export const ALL_GLOBAL_SOURCES = [
   princetonMavo,
   bauhausNewBauhausChicago,
   momaTorresGarcia,
+  bauhausPalestineSharon,
+  unescoTelAvivWhiteCity,
+  momaHerbertBayer,
+  momaBauhaus1938,
+  momaSternCoppola,
 ];
 
 export const globalSourcesRegistry = Object.fromEntries(
