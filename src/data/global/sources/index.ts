e7e32471080a88612f78a622349f52ed871c3a59
Bergtripper <albertocomini@gmail.com>
2026-09-25 21:54:29 +0200

    feat(1933): register historical fracture sources

--- src/data/global/sources/index.ts
+++ src/data/global/sources/index.ts
@@ -1,6 +1,9 @@
+import { bauhausAfter1933 } from './bauhaus-after-1933';
 import { bauhausBergnerMexico } from './bauhaus-bergner-mexico';
+import { bauhausClosure1933 } from './bauhaus-closure-1933';
 import { bauhausNewBauhausChicago } from './bauhaus-new-bauhaus-chicago';
 import { bauhausPalestineSharon } from './bauhaus-palestine-sharon';
+import { dhmHitlerChancellor1933 } from './dhm-hitler-chancellor-1933';
 import { kanagawaMurayama2011 } from './kanagawa-murayama-2011';
 import { martDeperoNewYork } from './mart-depero-new-york';
 import { metNewYorkDada } from './met-new-york-dada';
@@ -17,9 +20,12 @@ import { princetonMavo } from './princeton-mavo';
 import { unescoTelAvivWhiteCity } from './unesco-tel-aviv-white-city';
 
 export {
+  bauhausAfter1933,
   bauhausBergnerMexico,
+  bauhausClosure1933,
   bauhausNewBauhausChicago,
   bauhausPalestineSharon,
+  dhmHitlerChancellor1933,
   kanagawaMurayama2011,
   martDeperoNewYork,
   metNewYorkDada,
@@ -37,6 +43,9 @@ export {
 };
 
 export const ALL_GLOBAL_SOURCES = [
+  dhmHitlerChancellor1933,
+  bauhausClosure1933,
+  bauhausAfter1933,
   kanagawaMurayama2011,
   princetonMavo,
   bauhausNewBauhausChicago,
