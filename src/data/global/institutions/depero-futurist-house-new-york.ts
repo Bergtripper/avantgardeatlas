20028d40e3fd3ace8d3bcf161e25d5af79693cb8
Bergtripper <albertocomini@gmail.com>
2026-09-25 21:40:45 +0200

    feat(global): add depero-futurist-house-new-york.ts

---
+++ src/data/global/institutions/depero-futurist-house-new-york.ts
@@ -0,0 +1,15 @@
+import { GlobalEntity } from '../types';
+
+export const deperoFuturistHouseNewYork: GlobalEntity = {
+  id: 'depero-futurist-house-new-york',
+  name: 'Depero’s Futurist House, New York',
+  kind: 'institution',
+  hubId: 'new-york',
+  startYear: 1928,
+  endYear: 1930,
+  movementLinks: ['futurism'],
+  media: ['advertising', 'graphic-design', 'publishing', 'typography', 'exhibition-design'],
+  summary:
+    'The New York branch of Depero’s Casa d’Arte Futurista, created to bring Futurist applied art, graphics, advertising and design into the American commercial environment.',
+  sourceIds: ['mart-depero-new-york', 'moma-depero'],
+};
