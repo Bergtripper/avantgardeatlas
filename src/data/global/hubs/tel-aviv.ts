6dd54904c3790cd6b53a3721f7720e1017c9c838
Bergtripper <albertocomini@gmail.com>
2026-09-25 21:09:12 +0200

    feat(global): add tel-aviv.ts

---
+++ src/data/global/hubs/tel-aviv.ts
@@ -0,0 +1,13 @@
+import { GlobalHub } from '../types';
+
+export const telAvivHub: GlobalHub = {
+  id: 'tel-aviv',
+  name: 'Tel Aviv',
+  country: 'Mandatory Palestine',
+  latitude: 32.0853,
+  longitude: 34.7818,
+  activeEras: [{ start: 1931, end: 1940 }],
+  summary:
+    'A major site where architects trained in Europe translated modernist principles into a distinct Mediterranean and local urban context during the rapid growth of the 1930s.',
+  sourceIds: ['bauhaus-palestine-sharon', 'unesco-tel-aviv-white-city'],
+};
