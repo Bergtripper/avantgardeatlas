23bef17e09fbbf8eb5f15ededfc30bb528e80921
Bergtripper <albertocomini@gmail.com>
2026-09-25 22:46:18 +0200

    feat(print): register magazine editors

--- src/data/global/people/index.ts
+++ src/data/global/people/index.ts
@@ -1,24 +1,33 @@
 import { ariehSharon } from './arieh-sharon';
+import { francisPicabia } from './francis-picabia';
 import { greteStern } from './grete-stern';
 import { horacioCoppola } from './horacio-coppola';
 import { joaquinTorresGarcia } from './joaquin-torres-garcia';
+import { lajosKassak } from './lajos-kassak';
 import { lenaBergner } from './lena-bergner';
 import { manRay } from './man-ray';
 import { richardHuelsenbeck } from './richard-huelsenbeck';
+import { theoVanDoesburg } from './theo-van-doesburg';
 import { tomoyoshiMurayama } from './tomoyoshi-murayama';
 
 export {
   ariehSharon,
+  francisPicabia,
   greteStern,
   horacioCoppola,
   joaquinTorresGarcia,
+  lajosKassak,
   lenaBergner,
   manRay,
   richardHuelsenbeck,
+  theoVanDoesburg,
   tomoyoshiMurayama,
 };
 
 export const ALL_GLOBAL_PEOPLE = [
+  theoVanDoesburg,
+  lajosKassak,
+  francisPicabia,
   tomoyoshiMurayama,
   joaquinTorresGarcia,
   lenaBergner,
