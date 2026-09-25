400ede03ca02bb2f1f0cf99575d2c38650de74a8
Bergtripper <albertocomini@gmail.com>
2026-09-25 22:45:22 +0200

    feat(print): add kassak-ma-network.ts

---
+++ src/data/global/sources/kassak-ma-network.ts
@@ -0,0 +1,10 @@
+import { GlobalSource } from '../types';
+
+export const kassakMaNetwork: GlobalSource = {
+  id: 'kassak-ma-network',
+  title: 'Kassák!',
+  publisher: 'Kassák Museum',
+  url: 'https://kassakmuzeum.hu/hu/kassak',
+  sourceType: 'museum',
+  note: 'Used for MA’s Viennese exile period and Kassák’s international network with Schwitters, Tzara, Lissitzky, Arp and others.',
+};
