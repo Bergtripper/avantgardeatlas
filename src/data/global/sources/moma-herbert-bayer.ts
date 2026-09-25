c1acddd56f11c026f6dfd301d0b9a619f13500df
Bergtripper <albertocomini@gmail.com>
2026-09-25 21:09:38 +0200

    feat(global): add moma-herbert-bayer.ts

---
+++ src/data/global/sources/moma-herbert-bayer.ts
@@ -0,0 +1,10 @@
+import { GlobalSource } from '../types';
+
+export const momaHerbertBayer: GlobalSource = {
+  id: 'moma-herbert-bayer',
+  title: 'Herbert Bayer',
+  publisher: 'The Museum of Modern Art',
+  url: 'https://www.moma.org/artists/399',
+  sourceType: 'museum',
+  note: 'Used for Bayer’s 1938 emigration to the United States and his role in the Bauhaus exhibition at MoMA.',
+};
